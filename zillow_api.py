c02b992124ddf723eedc1fe1b14491f20bdbe1c3
Austin Schey <aschey13@gmail.com>
2019-12-01 12:04:57 -0600

    make api more robust, add more data

--- zillow_api.py
+++ zillow_api.py
@@ -1,7 +1,7 @@
 import requests
-import json
 import random
 import time
+import json
 
 def float_range(start, stop=None, step=None):
     if stop == None:
@@ -21,12 +21,14 @@ max_east = -87.608
 
 unit_delta_y = 0.02
 unit_delta_x = 0.04
+num_retries = 5
 
 listings = []
 ids = []
 url = 'https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState={"pagination":{currentPage: %s},"mapBounds":{"west":%s,"east":%s,"south":%s,"north":%s},"isMapVisible":true,"mapZoom":14,"filterState":{"isPreMarketForeclosure":{"value":false},"isPreMarketPreForeclosure":{"value":false},"isMakeMeMove":{"value":false}},"isListVisible":true}'
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'}
-
+iters = ((max_north - min_south) / unit_delta_y) * ((max_east - min_west) / unit_delta_x) * 20
+count = 0
 with open('results.json', 'w') as f:
     for lon in float_range(min_south, max_north, unit_delta_y):
         south = lon - (unit_delta_y * 0.1)
@@ -37,12 +39,23 @@ with open('results.json', 'w') as f:
             east = lat + unit_delta_x
 
             for current_page in range(1, 21):
-                print(f'\nPage {current_page} \nNorth {north} \nSouth {south} \nEast {east}\nWest {west}')
-                res = requests.get(url % (current_page, west, east, south, north), headers=headers)
-                data = json.loads(res.content)
+                print(f'\n{(count / iters):.4%} done')
+                print(f'Page {current_page} \nNorth {north} \nSouth {south} \nEast {east}\nWest {west}')
+                
+                data = None
+                for i in range(num_retries):
+                    try:
+                        data = requests.get(url % (current_page, west, east, south, north), headers=headers).json()
+                        break
+                    except:
+                        print(f'retry count {i}')
+                        if i == num_retries - 1:
+                            raise
+                        else:
+                            continue
                 if 'pagination' in data['queryState'] and data['queryState']['pagination']['currentPage'] != current_page:
                     break
-
+                
                 results = data['searchResults']['listResults']
                 print(f'Found {len(results)} records')
                 filtered = [result for result in results if result['id'] not in ids]
@@ -52,5 +65,5 @@ with open('results.json', 'w') as f:
                 listings.extend(filtered)
                 ids.extend(new_ids)
                 time.sleep(random.random() * random.randrange(1, 5))
-
+                count += 1
     json.dump(listings, f, indent=2)
